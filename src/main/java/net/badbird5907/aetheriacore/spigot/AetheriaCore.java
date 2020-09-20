44b2f822625412216ff24f034872f28680d934cf
DominicCM <34419970+DominicCM@users.noreply.github.com>
2020-09-19 21:56:51 -0400

    Test
    did things

--- src/main/java/net/badbird5907/aetheriacore/spigot/AetheriaCore.java
+++ src/main/java/net/badbird5907/aetheriacore/spigot/AetheriaCore.java
@@ -15,7 +15,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AetheriaCore extends JavaPlugin {
-
+    //This is a change by Dom as a test. :D
     public double togglepvp;
 
     private static AetheriaCore plugin;
