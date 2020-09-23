6999ee596427ff19786fd0d7bd5ab03906c8ac11
Badbird-5907 <evan.lele.yu2@gmail.com>
2020-09-22 21:32:30 -0400

    im dumber than i thought, also discordsrv listener fix?

--- src/main/java/net/badbird5907/aetheriacore/spigot/events/OnDiscordMessageRecieved.java
+++ src/main/java/net/badbird5907/aetheriacore/spigot/events/OnDiscordMessageRecieved.java
@@ -2,6 +2,7 @@ package net.badbird5907.aetheriacore.spigot.events;
 
 import github.scarsz.discordsrv.api.Subscribe;
 import github.scarsz.discordsrv.api.events.DiscordGuildMessagePreProcessEvent;
+import github.scarsz.discordsrv.api.events.DiscordGuildMessageReceivedEvent;
 import net.badbird5907.aetheriacore.spigot.AetheriaCore;
 import net.badbird5907.aetheriacore.spigot.api.StaffChatMessage;
 
@@ -12,7 +13,7 @@ public class OnDiscordMessageRecieved{
     }
 
     @Subscribe
-    public void onDiscordChat(DiscordGuildMessagePreProcessEvent event) {
+    public void onDiscordChat(DiscordGuildMessageReceivedEvent event) {
         if(event.getChannel().equals(plugin.getConfig().getString("StaffChat-Channel"))){
             StaffChatMessage.sendmessage(event.getAuthor().toString(), event.getMessage().toString());
         }
