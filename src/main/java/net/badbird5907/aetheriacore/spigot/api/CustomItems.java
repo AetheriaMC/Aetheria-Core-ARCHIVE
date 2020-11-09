957d59a19922a4d70e9446ca6b5d460bf6268853
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2020-11-09 11:18:26 -0500

    custom items api

---
+++ src/main/java/net/badbird5907/aetheriacore/spigot/api/CustomItems.java
@@ -0,0 +1,14 @@
+package net.badbird5907.aetheriacore.spigot.api;
+
+import net.badbird5907.aetheriacore.spigot.util.itemtypes;
+import org.bukkit.inventory.ItemStack;
+
+public class CustomItems {
+    public ItemStack get(String str){
+        ItemStack item = itemtypes.customitems.get(str);
+        return item;
+    }
+    public void put(String str, ItemStack itemStack){
+        itemtypes.customitems.put(str, itemStack);
+    }
+}
